e3ef46f2cc2c467c6e226d79efa6240686fd28d0
arya_shivam <aryashivam54@gmail.com>
2025-06-27 08:40:59 +0000

    logger added

--- server/express.js
+++ server/express.js
@@ -1,10 +1,35 @@
-const express = require('express')
+import express from 'express'
 const app = express()
 const port = 3000;
 app.use(express.json())
 
 const items=[]
 let id =1
+
+// Added advanced logging using morgan winston
+
+import logger from "./logger.js";
+import morgan from "morgan";
+
+const morganFormat = ":method :url :status :response-time ms";
+
+app.use(
+  morgan(morganFormat, {
+    stream: {
+      write: (message) => {
+        const logObject = {
+          method: message.split(" ")[0],
+          url: message.split(" ")[1],
+          status: message.split(" ")[2],
+          responseTime: message.split(" ")[3],
+        };
+        logger.info(JSON.stringify(logObject));
+      },
+    },
+  })
+);
+
+
 
 app.post('/teas',(req,res)=>{
     const {name,price}=req.body;
