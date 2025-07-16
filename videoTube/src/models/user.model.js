014b45ccc7a6c88b83258782f32df4be500001ef
arya_shivam <aryashivam54@gmail.com>
2025-07-16 17:44:47 +0000

    Added Pre hook and methods for user model

--- videoTube/src/models/user.model.js
+++ videoTube/src/models/user.model.js
@@ -1,4 +1,11 @@
 import mongoose, { Schema } from "mongoose";
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+
+dotenv.config({
+    path:'src/.env'
+})
 
 const UserSchema = new Schema(
     {
@@ -47,5 +54,39 @@ const UserSchema = new Schema(
     },
     {timestamps:true}
 )
+
+UserSchema.pre("save",async function(next){
+    if(!this.modified("password")) return next()
+
+    this.password = bcrypt.hash(this.password,10);
+    
+    next()
+})
+
+UserSchema.methods.isPasswordCorrect=async function(password){
+    return await bcrypt.compare(password,this.password)
+}
+
+UserSchema.methods.generateAccessToken=function(){
+    // short lived tokens
+    return jwt.sign({
+        _id:this._id,
+        email:this.email,
+        fullname:this.fullname
+    },
+    process.env.JWT_ACCESS_SECRET,
+    process.env.JWT_ACCESS_EXPIRY_TIME)
+}
+
+UserSchema.methods.generateRefreshToken=function(){
+    // long lived tokens
+    return jwt.sign({
+        _id:this._id,
+        email:this.email,
+        fullname:this.fullname
+    },
+    process.env.JWT_REFRESH_SECRET,
+    process.env.JWT_REFRESH_EXPIRY_TIME)
+}
 
 export const User = new mongoose.model("User",UserSchema)
